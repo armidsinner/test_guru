815250aaa65bc4e85092f274f06fff1648de70e5
armidsinner <69764302+armidsinner@users.noreply.github.com>
2021-05-27 23:48:29 +0300

    Add files via upload

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,9 @@ module TestGuru
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    VERSION = '0.1.0'.freeze
     config.active_record.raise_in_transactional_callbacks = true
-    config.active_record.default_timezone = :local
+    config.timezone = 'Moscow'
+    config.i18n.default_locale = :ru
   end
 end
