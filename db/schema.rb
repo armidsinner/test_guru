8395846e631cfd692c1acc78a3bda7dde76248b7
armidsinner <69764302+armidsinner@users.noreply.github.com>
2021-06-10 13:00:04 +0300

    Add files via upload

--- db/schema.rb
+++ db/schema.rb
@@ -11,41 +11,43 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210609221143) do
+ActiveRecord::Schema.define(version: 20210610095633) do
 
   create_table "answers", force: :cascade do |t|
-    t.string   "title",                     null: false
-    t.integer  "answer_id"
-    t.boolean  "correct",    default: true
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
+    t.string   "title",                      null: false
+    t.integer  "question_id",                null: false
+    t.boolean  "correct",     default: true
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
   create_table "categories", force: :cascade do |t|
     t.string   "title",      null: false
-    t.integer  "level"
+    t.integer  "level",      null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "questions", force: :cascade do |t|
-    t.string   "title"
-    t.integer  "categorie_id"
-    t.integer  "answer_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.string   "title",       null: false
+    t.integer  "category_id", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "tests", force: :cascade do |t|
-    t.string   "title",                  null: false
-    t.integer  "level",      default: 0
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.string   "title",                   null: false
+    t.integer  "level",       default: 0
+    t.integer  "category_id",             null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
   end
 
   create_table "users", force: :cascade do |t|
     t.string   "name",       null: false
     t.integer  "score"
+    t.string   "email",      null: false
+    t.string   "password",   null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
