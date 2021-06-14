e46ba15d80bd0bd2cb8fd7d1440d00ccaffb54b1
armidsinner <69764302+armidsinner@users.noreply.github.com>
2021-06-14 17:29:17 +0300

    Delete user.rb

--- user.rb
+++
@@ -1,9 +0,0 @@
-class User < ActiveRecord::Base
-  attr_accessor :tests
-
-  def tests_used_to_solve(needed_level)
-    @tests.each do |test|
-      puts ('Тесты соответствующего уровня: ' + test.title.to_s) if test.level == needed_level
-    end
-  end
-end
