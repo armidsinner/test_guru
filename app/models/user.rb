4342cfa53c13c2c85bcb4fed5c2d57900da71a57
armidsinner <69764302+armidsinner@users.noreply.github.com>
2021-06-16 03:09:18 +0300

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,5 @@
 class User < ActiveRecord::Base
   def tests_used_to_solve(needed_level)
-    # User.joins('Join tests ON user.id = tests.id').where('tests.level = ?',
-    #  level = needed_level)  .joins('Join tests ON test.id = results.test_id').where(tests: { level: needed_level })
     User.joins('Join results ON users.id = results.user_id').joins('Join tests ON tests.id = results.test_id').where(tests: { level: needed_level }).pluck(:title)
   end
 end
