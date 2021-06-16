3bb110213e26d8a62042df61a6aa6085d3bd4eb6
armidsinner <69764302+armidsinner@users.noreply.github.com>
2021-06-16 03:03:59 +0300

    Add files via upload

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,7 @@
 class Test < ActiveRecord::Base
   def self.tests_with_category(needed_category)
-    Test.joins('Join categories ON tests.category_id = categories.id').where('categories.title = ?',
-    title = needed_category).order(title: :desc)
+    joins('Join categories ON tests.category_id = categories.id').where(categories: { title: needed_category }).order(title: :desc).pluck(:title)
   end
 end
+
+# pp Test.tests_with_category('Math')
