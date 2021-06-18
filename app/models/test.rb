fd62bf42a4504fc9c0c560b4e4ec23d89d145d45
armidsinner <69764302+armidsinner@users.noreply.github.com>
2021-06-18 14:54:19 +0300

    Add files via upload

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,10 @@
 class Test < ActiveRecord::Base
+  belongs_to :category
+  has_many :questions
+  has_many :tests_users
+  has_many :users, through: :tests_users
+  belongs_to :author, :class_name => 'User'
+
   def self.tests_with_category(needed_category)
     joins('Join categories ON tests.category_id = categories.id').where(categories: { title: needed_category }).order(title: :desc).pluck(:title)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ActiveRecord::Base
+  has_many :tests_users
+  has_many :tests, through: :tests_users
+  has_many :tests, :foreign_key => :author_id, :class_name => 'Test'
+
   def tests_used_to_solve(needed_level)
-    Test.joins('Join results ON tests.id = results.test_id').where(level: needed_level ).where(results: { user_id: id })
+    Test.joins('Join results ON tests.id = results.test_id').where(level: needed_level).where(results: { user_id: id })
   end
 end
