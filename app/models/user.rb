3bc3d6f0d8e12358e2f35e2be44771a0db3bf641
armidsinner <69764302+armidsinner@users.noreply.github.com>
2021-06-19 04:32:33 +0300

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,6 @@ class User < ActiveRecord::Base
   has_many :tests, :foreign_key => :author_id, class_name: 'Test', dependent: :delete_all
 
   def tests_used_to_solve(needed_level)
-    #Test.joins('Join tests_user ON tests.id = tests_users.test_i').where(level: needed_level).where(tests_users: { user_id: id })
     Test.joins(:tests_users).where(tests_users: { user_id: id }).where(tests: { level: needed_level })
   end
 end
-
