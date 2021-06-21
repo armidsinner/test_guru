d1e61e64922b019d21edfd822d9ade240df30106
armidsinner <69764302+armidsinner@users.noreply.github.com>
2021-06-21 16:40:50 +0300

    Add files via upload

--- db/migrate/20210618111519_create_tests_users_join_table.rb
+++ db/migrate/20210618111519_create_tests_users_join_table.rb
@@ -1,8 +1,8 @@
 class CreateTestsUsersJoinTable < ActiveRecord::Migration
   def change
     create_join_table :users, :tests do |t|
-      t.index [:user_id, :test_id], unique: true 
-      t.index [:test_id, :user_id], unique: true 
+      t.index %i[user_id test_id], unique: true
+      t.index %i[test_id user_id], unique: true
     end
   end
 end
