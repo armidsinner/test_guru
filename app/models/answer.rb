2d13992c7d31801191ef82088f52aec42e229948
armidsinner <69764302+armidsinner@users.noreply.github.com>
2021-06-22 20:58:47 +0300

    Add files via upload

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,12 +1,14 @@
 class Answer < ActiveRecord::Base
   belongs_to :question
 
-  validates :title, presence: true
-  validate :amount_of_answers?
+  validates :title, presence: true 
+  validate :amount_of_answers?, on: :create
 
   scope :correct, -> { where(correct: true) }
 
   def amount_of_answers?
-    errors.add(:question, "The amount of answers must be between 1 and 4.") unless question.answers.count <= 3 
+    if question.answers.count > 3 
+      errors.add(:question, "The amount of answers must be between 1 and 4.")  
+    end
   end
 end
