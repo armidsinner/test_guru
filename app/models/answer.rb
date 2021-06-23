1284aa497f2fa5dffe04500cc258e43b773b4134
armidsinner <69764302+armidsinner@users.noreply.github.com>
2021-06-24 00:45:24 +0300

    Update answer.rb

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,11 +2,11 @@ class Answer < ActiveRecord::Base
   belongs_to :question
 
   validates :title, presence: true 
-  validate :amount_of_answers?, on: :create
+  validate :amount_of_answers, on: :create
 
   scope :correct, -> { where(correct: true) }
 
-  def amount_of_answers?
+  def amount_of_answers
     if question.answers.count > 3 
       errors.add(:question, "The amount of answers must be between 1 and 4.")  
     end
