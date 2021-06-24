0a2000e13b4ee26ebbcdaafae13e2c3432973b44
armidsinner <69764302+armidsinner@users.noreply.github.com>
2021-06-24 20:05:20 +0300

    Add files via upload

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,39 @@
+class QuestionsController < ApplicationController
+  before_action :find_test #, only: %i[index create new]
+
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
+
+  def index 
+    render inline: 'Вопросы для заданного теста: <%= @test.questions.inspect %>'
+  end
+
+  def show 
+    render inline: 'Вопрос с заданным ID: <%= @test.questions.find(params[:id]).inspect %>'
+  end
+
+  def new
+
+  end
+  
+  def create
+    @question = @test.questions.create!(question_params)
+  end
+
+  def destroy
+    @question.destroy
+  end
+
+  private
+
+  def find_test
+    @test = Test.find(params[:test_id])
+  end 
+
+  def question_params
+    params.require(:question).permit(:title)
+  end
+
+  def rescue_with_question_not_found
+    render plain: 'Question was not found'
+  end 
+end
