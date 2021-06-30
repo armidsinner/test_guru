8b417b700f77934b6760463a7b6b984f37835c2c
armidsinner <69764302+armidsinner@users.noreply.github.com>
2021-06-30 13:40:05 +0300

    Add files via upload

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,16 +1,14 @@
 module QuestionsHelper
   def question_header(question)
-    @test = Test.find(params[:test_id])
     if question.new_record? 
-      'Создать вопрос для теста ' + @test.title
+      'Создать вопрос' 
     else  
-      'Редактировать вопрос ' + question.title + ' для теста ' + @test.title
+      'Редактировать вопрос ' + question.title + ' для теста ' + question.test.title
     end
   end
 
   def current_year
-    time = Time.current
-    time.year
+    Time.current.year
   end
 
   def guthub_url(author, repo, title)
