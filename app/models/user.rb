cd5a347954a344b985b51ca6fe613bb94d1f21bb
armidsinner <69764302+armidsinner@users.noreply.github.com>
2021-07-05 15:00:53 +0300

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,14 +5,15 @@ class User < ActiveRecord::Base
   has_many :test_passages, dependent: :destroy
   has_many :tests, through: :test_passages
   has_many :authored_tests, foreign_key: :author_id, class_name: 'Test'
-
-  scope :tests_used_to_solve, ->(needed_level) { joins(:test_passages).where(tests_passages: { user_id: id }).where(tests: { level: needed_level }) }
-
+  
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true, 
                                     format: { with: URI::MailTo::EMAIL_REGEXP,
                                               message: 'Неверный формат. 
                                               Формат почты должен соответствовать: name@post.com' }
+
+  scope :tests_used_to_solve, ->(needed_level) { joins(:test_passages).where(tests_passages: { user_id: id }).where(tests: { level: needed_level }) }
+
   has_secure_password
 
   def test_passage(test)
