7a2a27fc055f9a621435d0a7250b4600361755e2
armidsinner <69764302+armidsinner@users.noreply.github.com>
2021-07-06 23:02:55 +0300

    Add files via upload

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,11 @@
 Rails.application.routes.draw do
 
-  get :login, to: 'sessions#new'
-  get :signup, to: 'users#new'
-  delete :logout, to: 'sessions#destroy'
+  devise_for :users, controllers: { sessions: "sessions" }, 
+                     path: :gurus, path_names: { sign_in: :login, sign_out: :logout }
 
   root 'tests#index'
 
-  resources :users, only: :create
-  resources :sessions, only: :create
- 
-  resources :tests do
-    resources :questions, shallow: true, except: :index do
-      resources :answers, shallow: true, except: :index
-    end
+  resources :tests, only: :index do
     member do
       post :start
     end
@@ -21,6 +14,14 @@ Rails.application.routes.draw do
   resources :test_passages, only: %i[show update] do
     member do
       get :result
+    end
+  end
+
+  namespace :admin do 
+    resources :tests do
+      resources :questions, shallow: true, except: :index do
+        resources :answers, shallow: true, except: :index
+      end
     end
   end
 end
