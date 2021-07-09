7fe01ede046823ce50dbb2597832942387d513e9
armidsinner <69764302+armidsinner@users.noreply.github.com>
2021-07-09 14:55:11 +0300

    Add files via upload

--- app/controllers/gists_controller.rb
+++ app/controllers/gists_controller.rb
@@ -2,11 +2,10 @@ class GistsController < ApplicationController
   before_action :find_test_passage, only: :create
 
   def create
-    @result = GistQuestionService.new(@test_passage.current_question).call
-    #Если ссылка на созданный гист сущестует, значит гист был успешно отправлен
-    if @result.get_html_url.present?
-      current_user.gists.create!(question: @test_passage.current_question, url: @result.get_html_url)
-      flash[:notice] = "Gist был успешно создан! #{view_context_link}" 
+    result = GistQuestionService.new(@test_passage.current_question).call
+    if result.success?
+      current_user.gists.create!(question: @test_passage.current_question, url: result.get_html_url)
+      flash[:notice] = "Gist был успешно создан! #{view_context_link(result)}" 
     else 
       flash[:alert]  = 'Ошибка при создании gist!'
     end
@@ -19,7 +18,7 @@ class GistsController < ApplicationController
     @test_passage = TestPassage.find(params[:test_passage_id])
   end 
 
-  def view_context_link
-    view_context.link_to('Просмотреть созданный gist на GitHub', @result.get_html_url, target: "_blank")
+  def view_context_link(result)
+    view_context.link_to('Просмотреть созданный gist на GitHub', result.get_html_url, target: "_blank")
   end 
 end
