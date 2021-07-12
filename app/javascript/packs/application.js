d25cc987a137d339f37d0af7c13832584a065453
armidsinner <69764302+armidsinner@users.noreply.github.com>
2021-07-12 23:21:56 +0300

    Add files via upload

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,21 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
+
+const Check_confirmation = require('./check_password_coincidence.js')
+const compare_passwords = new Check_confirmation('user_password', 'user_password_confirmation')
+compare_passwords.compare()
+
+const Sorting = require('./sorting.js')
+const sorted_table = new Sorting('table', "sort-by-title")
+sorted_table.sort_table()
--- app/javascript/packs/sorting.js
+++ app/javascript/packs/sorting.js
@@ -1,76 +1,71 @@
 class Sorting {
-  #id
-  #sorting_type
+  id
+  sorting_type
   constructor(id, sorting_type) {
-    this.#id = id
-    this.#sorting_type = sorting_type
+    this.id = id
+    this.sorting_type = sorting_type
   }
 
   sort_table() {
-    element_id = this.#id
-    sorting_type = this.#sorting_type
+    element_id = this.id
+    sorting_type = this.sorting_type
     document.addEventListener('turbolinks:load', function() {
       let control = document.getElementById(sorting_type)
 
       if (control) { control.addEventListener('click',sortRowsByTitle) }
     })
+  }
 
-    function sortRowsByTitle() {
-      let table = document.getElementById(element_id)
-      let rows = table.querySelectorAll('tr')
+  sortRowsByTitle() {
+    let table = document.getElementById(element_id)
+    let rows = table.querySelectorAll('tr')
+    let sortedRows = []
 
-      let sortedRows = []
-
-      for ( let i = 1; i < rows.length; i++) {
-        sortedRows.push(rows[i])
-      }
-
-      let arrowUp = this.querySelector('.octicon-arrow-up')
-      let arrowDown = this.querySelector('.octicon-arrow-down')
-
-      if (this.querySelector('.octicon-arrow-up').classList.contains('hide')) {
-        sortedRows.sort(compareRowsAsc)
-        this.querySelector('.octicon-arrow-up').classList.remove('hide')
-        this.querySelector('.octicon-arrow-down').classList.add('hide')
-      } else {
-        sortedRows.sort(compareRowsDesc)
-        this.querySelector('.octicon-arrow-up').classList.add('hide')
-        this.querySelector('.octicon-arrow-down').classList.remove('hide')
-      }
-
-      let sortedTable = document.createElement('table')
-      
-
-      sortedTable.setAttribute('id', 'table')
-      sortedTable.appendChild(rows[0])
-
-      for ( let i = 0; i < sortedRows.length; i++) {
-        sortedTable.appendChild(sortedRows[i])
-      }
-
-      table.parentNode.replaceChild(sortedTable, table)
+    for ( let i = 1; i < rows.length; i++) {
+      sortedRows.push(rows[i])
     }
 
-    function compareRowsAsc(row1, row2) {
-      let title_to_compare1 = row1.querySelector('td').textContent
-      let title_to_compare2 = row2.querySelector('td').textContent
+    let arrowUp = this.querySelector('.octicon-arrow-up')
+    let arrowDown = this.querySelector('.octicon-arrow-down')
 
-      if (title_to_compare1 < title_to_compare2) {return -1}
-      if (title_to_compare1 > title_to_compare2) {return 1}
-      return 0
+    if (this.querySelector('.octicon-arrow-up').classList.contains('hide')) {
+      sortedRows.sort(compareRowsAsc)
+      this.querySelector('.octicon-arrow-up').classList.remove('hide')
+      this.querySelector('.octicon-arrow-down').classList.add('hide')
+    } else {
+      sortedRows.sort(compareRowsDesc)
+      this.querySelector('.octicon-arrow-up').classList.add('hide')
+      this.querySelector('.octicon-arrow-down').classList.remove('hide')
     }
 
-    function compareRowsDesc(row1, row2) {
-      let title_to_compare1 = row1.querySelector('td').textContent
-      let title_to_compare2 = row2.querySelector('td').textContent
+    let sortedTable = document.createElement('table')
+    sortedTable.setAttribute('id', 'table')
+    sortedTable.appendChild(rows[0])
 
-      if (title_to_compare1 < title_to_compare2) {return 1}
-      if (title_to_compare1 > title_to_compare2) {return -1}
-      return 0
+    for ( let i = 0; i < sortedRows.length; i++) {
+      sortedTable.appendChild(sortedRows[i])
     }
+
+    table.parentNode.replaceChild(sortedTable, table)
+  }
+
+  compareRowsAsc(row1, row2) {
+    let title_to_compare1 = row1.querySelector('td').textContent
+    let title_to_compare2 = row2.querySelector('td').textContent
+
+    if (title_to_compare1 < title_to_compare2) {return -1}
+    if (title_to_compare1 > title_to_compare2) {return 1}
+    return 0
+  }
+
+  compareRowsDesc(row1, row2) {
+    let title_to_compare1 = row1.querySelector('td').textContent
+    let title_to_compare2 = row2.querySelector('td').textContent
+
+    if (title_to_compare1 < title_to_compare2) {return 1}
+    if (title_to_compare1 > title_to_compare2) {return -1}
+    return 0
   }
 }
 
-
-const sorted_table = new Sorting('table', "sort-by-title")
-sorted_table.sort_table()
+module.exports = Sorting
