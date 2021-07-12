5656b63a5a098928edd099cc969f4e787343f0be
armidsinner <69764302+armidsinner@users.noreply.github.com>
2021-07-12 16:46:48 +0300

    Add files via upload

--- app/javascript/packs/check_password_coincidence.js
+++ app/javascript/packs/check_password_coincidence.js
@@ -1,37 +1,52 @@
-document.addEventListener('turbolinks:load', function () {
-  let control = document.querySelector('.check-coincidence')
+class Check_coincidence {   
+#first_element_to_compare_id
+#second_element_to_compare_id
+  constructor(first_element_to_compare_id, second_element_to_compare_id) {
+    this.#first_element_to_compare_id = first_element_to_compare_id
+    this.#second_element_to_compare_id = second_element_to_compare_id
+  }
+   
+  compare() {
+    id1 = this.#first_element_to_compare_id
+    id2 = this.#second_element_to_compare_id
+    document.addEventListener('turbolinks:load', function () {
+      let control = document.querySelector('.check-coincidence')
 
-  if (control) { control.addEventListener('input', passwordCheck) }
-})
+      if (control) { control.addEventListener('input', passwordCheck) }
+    })
 
-function passwordCheck() {
-  let password = document.getElementById('user_password')
-  let confirmation = document.getElementById('user_password_confirmation')
-  let match = document.querySelector('.octicon-check-circle').classList
-  let notMatch = document.querySelector('.octicon-alert').classList
+    function passwordCheck() {
+      let password = document.getElementById(id1)
+      let confirmation = document.getElementById(id2)
+      let match = document.querySelector('.octicon-check-circle').classList
+      let notMatch = document.querySelector('.octicon-alert').classList
 
-  if (confirmation.value) {
-    // Добавил проверку на длину пароля сразу в проверку совпадения
-    if (password.value == confirmation.value &&  password.value.length > 5) {
-      match.remove('hide')
-      notMatch.add('hide')
-      password.classList.add('border-green')
-      password.classList.remove('border-orange')
-      confirmation.classList.add('border-green')
-      confirmation.classList.remove('border-orange')
-    } else {
-      match.add('hide')
-      notMatch.remove('hide')
-      password.classList.remove('border-green')
-      password.classList.add('border-orange')
-      confirmation.classList.remove('border-green')
-      confirmation.classList.add('border-orange')
+      if (confirmation.value) {
+        if (password.value === confirmation.value) {
+          match.remove('hide')
+          notMatch.add('hide')
+          password.classList.add('border-green')
+          password.classList.remove('border-orange')
+          confirmation.classList.add('border-green')
+          confirmation.classList.remove('border-orange')
+        } else {
+          match.add('hide')
+          notMatch.remove('hide')
+          password.classList.remove('border-green')
+          password.classList.add('border-orange')
+          confirmation.classList.remove('border-green')
+          confirmation.classList.add('border-orange')
+        }
+
+      } else {
+        match.add('hide')
+        notMatch.add('hide')
+        password.classList.remove('border-orange')
+        confirmation.classList.remove('border-orange')
+      }
     }
-
-  } else {
-    match.add('hide')
-    notMatch.add('hide')
-    password.classList.remove('border-orange')
-    confirmation.classList.remove('border-orange')
   }
 }
+
+const compare_passwords = new Check_coincidence('user_password', 'user_password_confirmation')
+compare_passwords.compare()
--- app/javascript/packs/sorting.js
+++ app/javascript/packs/sorting.js
@@ -1,57 +1,73 @@
-document.addEventListener('turbolinks:load', function() {
-  let control = document.querySelector('.sort-by-title')
-
-  if (control) { control.addEventListener('click',sortRowsByTitle) }
-})
-
-function sortRowsByTitle() {
-  let table = document.querySelector('table')
-  let rows = table.querySelectorAll('tr')
-  let sortedRows = []
-
-  for ( let i = 1; i < rows.length; i++) {
-  sortedRows.push(rows[i])
+class Sorting {
+  #id
+  constructor(id) {
+    this.#id = id
   }
 
-  let arrowUp = this.querySelector('.octicon-arrow-up')
-  let arrowDown = this.querySelector('.octicon-arrow-down')
+  sort_table() {
+    element_id = this.#id
+    document.addEventListener('turbolinks:load', function() {
+      let control = document.querySelector('.sort-by-title')
 
-  if (this.querySelector('.octicon-arrow-up').classList.contains('hide')) {
-    sortedRows.sort(compareRowsAsc)
-    this.querySelector('.octicon-arrow-up').classList.remove('hide')
-    this.querySelector('.octicon-arrow-down').classList.add('hide')
-  } else {
-    sortedRows.sort(compareRowsDesc)
-    this.querySelector('.octicon-arrow-up').classList.add('hide')
-    this.querySelector('.octicon-arrow-down').classList.remove('hide')
+      if (control) { control.addEventListener('click',sortRowsByTitle) }
+    })
+
+    function sortRowsByTitle() {
+      let table = document.getElementById(element_id)
+      let rows = table.querySelectorAll('tr')
+
+      let sortedRows = []
+
+      for ( let i = 1; i < rows.length; i++) {
+        sortedRows.push(rows[i])
+      }
+
+      let arrowUp = this.querySelector('.octicon-arrow-up')
+      let arrowDown = this.querySelector('.octicon-arrow-down')
+
+      if (this.querySelector('.octicon-arrow-up').classList.contains('hide')) {
+        sortedRows.sort(compareRowsAsc)
+        this.querySelector('.octicon-arrow-up').classList.remove('hide')
+        this.querySelector('.octicon-arrow-down').classList.add('hide')
+      } else {
+        sortedRows.sort(compareRowsDesc)
+        this.querySelector('.octicon-arrow-up').classList.add('hide')
+        this.querySelector('.octicon-arrow-down').classList.remove('hide')
+      }
+
+      let sortedTable = document.createElement('table')
+      
+
+      sortedTable.setAttribute('id', 'table')
+      sortedTable.appendChild(rows[0])
+
+      for ( let i = 0; i < sortedRows.length; i++) {
+        sortedTable.appendChild(sortedRows[i])
+      }
+
+      table.parentNode.replaceChild(sortedTable, table)
+    }
+
+    function compareRowsAsc(row1, row2) {
+      let title_to_compare1 = row1.querySelector('td').textContent
+      let title_to_compare2 = row2.querySelector('td').textContent
+
+      if (title_to_compare1 < title_to_compare2) {return -1}
+      if (title_to_compare1 > title_to_compare2) {return 1}
+      return 0
+    }
+
+    function compareRowsDesc(row1, row2) {
+      let title_to_compare1 = row1.querySelector('td').textContent
+      let title_to_compare2 = row2.querySelector('td').textContent
+
+      if (title_to_compare1 < title_to_compare2) {return 1}
+      if (title_to_compare1 > title_to_compare2) {return -1}
+      return 0
+    }
   }
-
-  let sortedTable = document.createElement('table')
-
-  sortedTable.classList.add('table')
-  sortedTable.appendChild(rows[0])
-
-  for ( let i = 0; i < sortedRows.length; i++) {
-    sortedTable.appendChild(sortedRows[i])
-  }
-
-  table.parentNode.replaceChild(sortedTable, table)
 }
 
-function compareRowsAsc(row1, row2) {
-  let testTitle1 = row1.querySelector('td').textContent
-  let testTitle2 = row2.querySelector('td').textContent
 
-  if (testTitle1 < testTitle2) {return -1}
-  if (testTitle1 > testTitle2) {return 1}
-  return 0
-}
-
-function compareRowsDesc(row1, row2) {
-  let testTitle1 = row1.querySelector('td').textContent
-  let testTitle2 = row2.querySelector('td').textContent
-
-  if (testTitle1 < testTitle2) {return 1}
-  if (testTitle1 > testTitle2) {return -1}
-  return 0
-}
+const sorted_table = new Sorting('table')
+sorted_table.sort_table()
