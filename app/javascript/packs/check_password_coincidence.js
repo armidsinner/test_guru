1765ccc3c7e92118f35ef2e69f951caf0003a488
armidsinner <69764302+armidsinner@users.noreply.github.com>
2021-07-12 17:05:21 +0300

    Update check_password_coincidence.js

--- app/javascript/packs/check_password_coincidence.js
+++ app/javascript/packs/check_password_coincidence.js
@@ -1,42 +1,39 @@
-class Check_coincidence {   
+class Check_confirmation {   
 #first_element_to_compare_id
 #second_element_to_compare_id
-#check_type
   constructor(first_element_to_compare_id, second_element_to_compare_id, check_type) {
     this.#first_element_to_compare_id = first_element_to_compare_id
     this.#second_element_to_compare_id = second_element_to_compare_id
-    this.#check_type = check_type
   }
    
   compare() {
     id1 = this.#first_element_to_compare_id
     id2 = this.#second_element_to_compare_id
-    check_type = this.#check_type
     document.addEventListener('turbolinks:load', function () {
-      let control = document.getElementById(check_type)
+      let control = document.getElementById("check-coincidence")
 
       if (control) { control.addEventListener('input', passwordCheck) }
     })
 
     function passwordCheck() {
-      let password = document.getElementById(id1)
+      let user_input = document.getElementById(id1)
       let confirmation = document.getElementById(id2)
       let match = document.querySelector('.octicon-check-circle').classList
       let notMatch = document.querySelector('.octicon-alert').classList
 
       if (confirmation.value) {
-        if (password.value === confirmation.value) {
+        if (user_input.value === confirmation.value) {
           match.remove('hide')
           notMatch.add('hide')
-          password.classList.add('border-green')
-          password.classList.remove('border-orange')
+          user_input.classList.add('border-green')
+          user_input.classList.remove('border-orange')
           confirmation.classList.add('border-green')
           confirmation.classList.remove('border-orange')
         } else {
           match.add('hide')
           notMatch.remove('hide')
-          password.classList.remove('border-green')
-          password.classList.add('border-orange')
+          user_input.classList.remove('border-green')
+          user_input.classList.add('border-orange')
           confirmation.classList.remove('border-green')
           confirmation.classList.add('border-orange')
         }
@@ -44,12 +41,12 @@ class Check_coincidence {
       } else {
         match.add('hide')
         notMatch.add('hide')
-        password.classList.remove('border-orange')
+        user_input.classList.remove('border-orange')
         confirmation.classList.remove('border-orange')
       }
     }
   }
 }
 
-const compare_passwords = new Check_coincidence('user_password', 'user_password_confirmation', "check-coincidence")
+const compare_passwords = new Check_confirmation('user_password', 'user_password_confirmation')
 compare_passwords.compare()
