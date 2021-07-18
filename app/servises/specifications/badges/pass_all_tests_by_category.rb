e3d92f87516c7927c47d51afc394beb8aaf9d418
armidsinner <69764302+armidsinner@users.noreply.github.com>
2021-07-18 10:10:13 +0300

    Delete app/servises/specifications/badges directory

--- app/servises/specifications/badges/pass_all_tests_by_category.rb
+++
@@ -1,7 +0,0 @@
-class Specifications::Badges::PassAllTestsByCategory
-  def self.satisfied_by?(test_passage, badge)
-    test_passage.user.not_having?(badge) &&
-    test_passage.success == true &&  Test.all.by_category_id(badge.params).count ==
-    test_passage.user.test_passages.by_category_id(badge.params).no_repeat.count 
-  end
-end
