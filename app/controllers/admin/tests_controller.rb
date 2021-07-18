8cec7631e3902fe28341c68909ca4a630eebd97a
armidsinner <69764302+armidsinner@users.noreply.github.com>
2021-07-18 10:23:04 +0300

    Add files via upload

---
+++ app/controllers/admin/tests_controller.rb
@@ -0,0 +1,70 @@
+class Admin::TestsController < Admin::BaseController
+  before_action :find_tests, only: %i[index update_inline]
+  before_action :find_test, only: %i[show edit update destroy start update_inline] 
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_test_not_found
+
+  def index; end 
+
+  def show 
+    @questions = @test.questions
+  end
+
+  def new
+    @test = Test.new
+  end
+
+  def create 
+    @test = current_user.authored_tests.new(test_params)
+    if @test.save
+      redirect_to [:admin, @test]
+    else 
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @test.update(test_params)
+      redirect_to [:admin, @test]
+    else
+      render :edit 
+    end 
+  end
+
+  def update_inline 
+    if @test.update(test_params)
+      redirect_to admin_tests_path
+    else 
+      render :index
+    end
+  end
+
+  def destroy
+    @test.destroy
+    redirect_to admin_tests_path
+  end
+
+  def start 
+    current_user.tests.push(@test)
+    redirect_to current_user.test_passage(@test)
+  end
+  
+  private
+
+  def find_tests
+    @tests = Test.all
+  end
+
+  def find_test
+    @test = Test.find(params[:id])
+  end 
+
+  def test_params
+    params.require(:test).permit(:title, :level, :category_id, :time)
+  end
+
+  def rescue_with_test_not_found
+    render plain: 'Тест не найден'
+  end
+end
